126f3be0191ad5f892782122bfa57bf5e27e9d87
rahul2409 <37111352+rahul2409@users.noreply.github.com>
2023-05-05 18:34:16 +0530

    Deletion logic added. (#4)
    Co-authored-by: rahul2409 <rahul_nandrajog@yahoo.com>

--- src/main/java/com/springdemo/cruddemo/DAO/StudentDAO.java
+++ src/main/java/com/springdemo/cruddemo/DAO/StudentDAO.java
@@ -7,11 +7,13 @@ public interface StudentDAO {
 	
 	void save(Student theStudent);
 	
-	Student findById(int id);
+	Student findById(Integer id);
 	
 	List<Student> findAll();
 
 	List<Student> findByLastName(String theLastName);
 	
 	void update(Student theStudent);
+	
+	void delete(Integer id);
 }
